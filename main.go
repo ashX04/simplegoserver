ff0eab27aa2b7cdf1364cc1942ace9f5f47f59e4
ashX04 <ashu0499@hotmail.com>
2025-02-08 09:52:42 +0530

    all routes work

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello server!")
+	fileserver := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileserver)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fmt.Println("Starting server on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Error in parsing form : %v", err)
+		return
+	}
+	fmt.Println("Post request successful!")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Hello %s from %s", name, address)
 }
